861da8ed88879aa4c6876d66a14329dcfcdb9947
sebastianespina <sebastianespina9@gmail.com>
2025-03-13 15:15:43 -0300

    amigo secreto terminado

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById('amigo');
+    let nombreAmigo = inputAmigo.value;
+
+    if (!nombreAmigo) {
+        alert('Debe ingresar un nombre');
+        return;
+    }
+    amigo.push(nombreAmigo);
+    inputAmigo.value = '';
+    inputAmigo.focus();
+    renderizarAmigos();
+    
+}
+
+function renderizarAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+    
+    for (let i = 0; i < amigo.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigo[i];
+        listaAmigos.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if(amigo.length === 0) {
+        alert('Debe ingresar al menos dos amigos');
+        return;
+    }
+    let amigoSorteado = amigo[Math.floor(Math.random() * amigo.length)];
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+
+    let limpiarLista = document.getElementById('listaAmigos');
+    limpiarLista.innerHTML = '';
+}
+
